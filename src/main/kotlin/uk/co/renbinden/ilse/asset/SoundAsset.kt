92696b2171859c3a361d1ca3271058bfd0e8c873
Ren Binden <ren.binden@gmail.com>
2020-01-30 22:19:14 +0000

    Allow modifying sound volume

--- src/main/kotlin/uk/co/renbinden/ilse/asset/SoundAsset.kt
+++ src/main/kotlin/uk/co/renbinden/ilse/asset/SoundAsset.kt
@@ -3,10 +3,16 @@ package uk.co.renbinden.ilse.asset
 import org.w3c.dom.Audio
 
 
-class SoundAsset(val source: String) : Asset() {
+class SoundAsset(val source: String, volume: Double = 1.0, var loop: Boolean = false) : Asset() {
 
     private val audioInstances = mutableListOf<Audio>()
     override val isLoaded: Boolean = true
+
+    var volume: Double = volume
+        set(value) {
+            audioInstances.forEach { it.volume = value }
+            field = value
+        }
 
     init {
         onLoad()
@@ -15,7 +21,10 @@ class SoundAsset(val source: String) : Asset() {
     fun play() {
         val stoppedAudios = audioInstances.filter { audio -> audio.paused || audio.currentTime <= 0 || audio.ended  }
         if (stoppedAudios.isEmpty()) {
-            audioInstances.add(Audio(source))
+            val audio = Audio(source)
+            audio.volume = volume
+            audio.loop = loop
+            audioInstances.add(audio)
             play()
             return
         }
