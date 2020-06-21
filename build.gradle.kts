c1d58d293618eb41f3d94f1b5b56a2fb2ec0b309
Ren Binden <ren.binden@gmail.com>
2020-06-21 11:55:06 +0100

    Fix tile lookups, closes #35

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("js") version "1.3.71"
+    kotlin("js") version "1.3.72"
     maven
 }
 
--- src/main/kotlin/uk/co/renbinden/ilse/tiled/TileInstance.kt
+++ src/main/kotlin/uk/co/renbinden/ilse/tiled/TileInstance.kt
@@ -5,6 +5,6 @@ class TileInstance(gid: Int?) {
     val gid = gid ?: 0
     fun get(map: TiledMap): Tile? {
         val tileSet = map.getTileSet(this)
-        return tileSet.tiles.firstOrNull { it.id == gid }
+        return tileSet.tiles.firstOrNull { it.id + tileSet.firstGid == gid }
     }
 }
